00f0b2cc83a55b31291bf74d8e8af99407c2fbe9
slv-crkishore <crkishore@skilline.ai>
2023-03-09 09:53:08 +0530

    changed registration view to model view set

--- application/urls.py
+++ application/urls.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from application.views import LoginApiView, UserRegistrationView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register("register", UserRegistrationView, basename="registration")
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("login/", LoginApiView.as_view(), name="login"),
-    path('register/', UserRegistrationView.as_view(), name="registration")
+    # path('register/', UserRegistrationView.as_view(), name="registration")
 ]
--- application/views.py
+++ application/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -13,26 +14,26 @@ from django.contrib.auth import authenticate
 # Create your views here.
 
 
-class LoginApiView(TokenObtainPairView):
-    authentication_classes = [JWTAuthentication]
-
-    def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-
-        # queryset = UserModel.objects.all()
-        if user is not None:
-            serializer = TokenObtainPairSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors)
-
-# from rest_framework_simplejwt.views import TokenObtainPairView
-
-
 # class LoginApiView(TokenObtainPairView):
-#     serializer_class = TokenObtainPairSerializer
+#     authentication_classes = [JWTAuthentication]
+
+#     def post(self, request, *args, **kwargs):
+#         username = request.data.get('username')
+#         password = request.data.get('password')
+#         user = authenticate(username=username, password=password)
+
+#         # queryset = UserModel.objects.all()
+#         if user is not None:
+#             serializer = TokenObtainPairSerializer(user)
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+
+#         return Response(serializer.errors)
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class LoginApiView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
 
 
 # class CheckView(APIView):
@@ -42,21 +43,22 @@ class LoginApiView(TokenObtainPairView):
 #     def get(request, *args, **kwargs):
 #         return Response("message: logged in")
 
-class UserRegistrationView(APIView):
+class UserRegistrationView(ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
 
-    def post(self, request, *args, **kwargs):
-        # queryset = UserModel.objects.all()
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    # def post(self, request, *args, **kwargs):
+    #     # queryset = UserModel.objects.all()
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors)
+    #     return Response(serializer.errors)
 
-    def get(self, request):
-        queryset = UserModel.objects.all()
-        serializer = self.serializer_class(queryset, many=True)
+    # def get(self, request):
+    #     queryset = UserModel.objects.all()
+    #     serializer = self.serializer_class(queryset, many=True)
 
-        return Response(serializer.data)
+    #     return Response(serializer.data)
